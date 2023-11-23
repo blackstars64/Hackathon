cc22c55e80e6869911f38be4408d8724256dd494
Blackstars <nelson.64@live.fr>
2023-11-23 10:20:20 +0100

    start Hackathon

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>bien bien</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
