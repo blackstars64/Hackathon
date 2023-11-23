02e2f3cf29edc8c05d7d8ddb7325ad27878bdf47
Blackstars <nelson.64@live.fr>
2023-11-23 23:47:47 +0100

    musique de fond

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useLoaderData } from "react-router-dom";
 import AdventCalendar from "./components/AdventCalendar";
 import "./css/app.css";
+import AudioPlayer from "./utils/AudioPlayer";
+import backMusic from "../src/assets/songs/background-music.wav";
 
 const App = () => {
   const data = useLoaderData();
+  const audioSource = backMusic;
 
   return (
     <div className="app">
+      <AudioPlayer audioSrc={audioSource} />
       <h1>Calendrier de lAvent</h1>
       <AdventCalendar data={data} />
     </div>
