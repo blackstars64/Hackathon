ebfbb12e2b6ce3d58c7a677c469cf94fe8929f9f
Maxence Chevee <max.rothen@hotmail.fr>
2023-11-23 12:07:21 +0100

    first try calendar

---
+++ src/components/AdventCalendar.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const AdventCalendar = () => {
+  const [openedDoors, setOpenedDoors] = useState([]);
+
+  const handleDoorClick = (doorNumber) => {
+    if (!openedDoors.includes(doorNumber)) {
+      setOpenedDoors([...openedDoors, doorNumber]);
+    }
+  };
+
+  const renderDoors = () => {
+    const totalDoors = 25;
+    const doors = [];
+
+    for (let i = 1; i <= totalDoors; i++) {
+      const isDoorOpened = openedDoors.includes(i);
+
+      const doorStyle = isDoorOpened
+        ? {
+            backgroundImage: "url('../assets/images/advent-4623521_640.png')",
+          }
+        : {};
+
+      doors.push(
+        <div
+          key={i}
+          className={`door ${isDoorOpened ? "opened" : ""}`}
+          style={doorStyle}
+          onClick={() => handleDoorClick(i)}
+        >
+          {isDoorOpened ? "🎁" : i}
+        </div>
+      );
+    }
+
+    return doors;
+  };
+
+  return <div className="advent-calendar">{renderDoors()}</div>;
+};
+
+export default AdventCalendar;
