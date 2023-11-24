69230b05b3abaab8d6e0a4bff5c9ab3009036f53
Blackstars <nelson.64@live.fr>
2023-11-24 02:28:25 +0100

    localstorage

--- src/components/AdventCalendar.jsx
+++ src/components/AdventCalendar.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import "../css/AdventCalendar.css";
 import { Link } from "react-router-dom";
+import { useLocalStorage } from "usehooks-ts";
 
 const AdventCalendar = ({ data }) => {
-  const [openedDoors, setOpenedDoors] = useState([]);
+  const [openedDoors, setOpenedDoors] = useLocalStorage("doors", []);
 
   const handleDoorClick = (doorNumber) => {
     if (!openedDoors.includes(doorNumber)) {
