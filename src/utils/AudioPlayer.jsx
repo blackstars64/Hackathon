33eeaddc3ea251c1c491382b104f25b3e8d9bdfe
Blackstars <nelson.64@live.fr>
2023-11-24 12:21:37 +0100

    volume

--- src/utils/AudioPlayer.jsx
+++ src/utils/AudioPlayer.jsx
@@ -19,7 +19,7 @@ const AudioPlayer = (props) => {
   const play = () => {
     const audio = audioRef.current;
     audio.play();
-    audio.volume = 0.2;
+    audio.volume = 0.1;
     setIsPlaying(true);
   };
 
