182daddb3c98f4ecd4af801aaffa08490c30ea1a
CHATELAIN JORDAN <jordan139@hotmail.fr>
2023-11-24 11:10:18 +0100

    add song laugh Nelson

--- src/components/Cadeaux.jsx
+++ src/components/Cadeaux.jsx
@@ -3,6 +3,10 @@ import Confetti from "react-confetti";
 import { useLoaderData, useParams } from "react-router-dom";
 import "../css/Cadeaux.css";
 import { Link } from "react-router-dom";
+
+// Importe le fichier sonore
+import rire_Nelson from "../../src/assets/songs/rire-Nelson.wav";
+
 function Cadeaux() {
   const data = useLoaderData();
   const { id } = useParams();
@@ -28,6 +32,12 @@ function Cadeaux() {
     };
   }, []);
 
+  // Fonction pour jouer le son
+  const jouerSon = () => {
+    const audio = new Audio(rire_Nelson);
+    audio.play();
+  };
+
   if (!dataCadeaux.length) {
     return <div>Cadeau non trouvé</div>;
   }
@@ -42,7 +52,7 @@ function Cadeaux() {
         {dataCadeaux[0].text}{" "}
       </p>
       <p className="yeux"> 👀</p>
-      <Link className="return" to="/">
+      <Link className="return" to="/" onClick={jouerSon}>
         Retour
       </Link>
     </div>
--- src/utils/AudioPlayer.jsx
+++ src/utils/AudioPlayer.jsx
@@ -17,7 +17,7 @@ const AudioPlayer = (props) => {
   const play = () => {
     const audio = audioRef.current;
     audio.play();
-    audio.volume = 0.1;
+    audio.volume = 1;
     setIsPlaying(true);
   };
 
